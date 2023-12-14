a2a11352bbcc036db46edff1caf71dd90e73a452
Ethan Hoyt <hoyt3141@gmail.com>
2023-12-14 17:38:09 -0500

    Implemented vec3 Class
    Implemented the vec3 class for general XYZ, RGB, etc. usage. Also implemented print updating in main for progress display.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,20 @@ int main() {
     int image_width = 256;
     int image_height = 256;
 
+    // ASCII color type, columns, rows, max color
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     // Image Rendering
     for (int row = 0; row < image_height; ++row) {
+        // Progress bar
+        std::clog << "\rScanlines Remaining: " << (image_height - row) << " " << std::flush;
         for (int col = 0; col < image_width; ++col) {
-            // r increases left to right, green increases top to bottom: ratio from 0-255
+            // r increases left to right, green increases top to bottom: ratio from 0.0-1.0
             float r = double(col) / (image_width-1);
             float g = double(row) / (image_height-1);
             float b = 0;
 
+            // essentially rounding to nearest int 0-255
             int ir = int(r * 255);
             int ig = int(g * 255);
             int ib = int(b * 255);
@@ -22,6 +26,7 @@ int main() {
             std::cout << ir << " " << ig << " " << ib << '\n';
         }
     }
+    std::clog << "\rDone.\n";
 
     return 0;
 }
---
+++ vec3.h
@@ -0,0 +1,111 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+// General class used for vectors, points, colors, etc.
+class vec3 {
+public:
+    double e[3];
+
+    // constructors
+    vec3() : e{0, 0, 0} {}
+    vec3(double d0, double e1, double e2) : e{e0, e1, e2} {}
+
+    // getters
+    double x() { return e[0]; }
+    double y() { return e[1]; }
+    double z() { return e[2]; }
+
+    // negative operator
+    vec3() operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+
+    // subscript operators
+    vec3 operator[](int i) const { return e[i]; }
+    vec3 operator[](int i) { return e[i]; }
+
+    // dot sum
+    vec3& operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    // not dot product
+    vec3& operator*=(double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    // why is this syntax so different from product?
+    vec3& operator/=(double t) {
+        return *this *= 1/t;
+    }
+
+    // different implementation from book
+    double length() const {
+        return e[0] + e[1] + e[2];
+    }
+
+    double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+};
+
+// just a vec3 alias, useful for geometric clarity in code
+using point3 = vec3;
+
+// Helper Utility Functions
+// print output
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+// sum initialization
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0]+v.e[0], u.e[1]+v.e[1], u.e[2]+v.e[2]);
+}
+
+// difference initialization
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0]-v.e[0], u.e[1]-v.e[1], u.e[2]-v.e[2]);
+}
+
+// product initialization
+inline vec3 operator*(double vec3& u, vec3& v) {
+    return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+// different syntax for division
+inline vec3 operator/(vec3 v, double t) {
+    return (1/t) * v;
+}
+
+// dot product
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0]*v.e[0] + u.e[1]*v.e[1] + u.e[2]*v.e[2];
+}
+
+// cross product
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u.e[1]*v.e[2] - u.e[2]*v.e[1], u.e[2]*v.e[0] - u.e[0]*v.e[2], u.e[0]*v.e[1] - u.e[1]*v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+
+#endif //VEC3_H
