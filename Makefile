1e84cb627fb0f43b6403d34d59d3f5caf47abaac
Ethan Hoyt <hoyt3141@gmail.com>
2024-07-23 17:32:52 -0400

    Implemented 14.1
    Generated the final render for the book!

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all:
 	g++ -std=c++20 -g ./*.cpp -Wall -O2 -o raytrace;
 
 run:
-	./raytrace > outputs/13.2.ppm;
+	./raytrace > outputs/14.1.ppm;
 
 clean:
 	rm -f raytrace;
--- main.cpp
+++ main.cpp
@@ -7,45 +7,66 @@
 #include "sphere.h"
 
 int main() {
-
-    // Environment Setup
     hittable_list world;
 
-    // Material Generation
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));                   // greenish
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));                   // mild blue
-    // If a shallow ray is fired at glass, it is reflected.
-    // If a relatively straight-on ray is fired, it is refracted:
-    // theta prime (refracted angle) > theta (incident angle) if refraction index of material is < 1.
-    // theta prime (refracted angle) < theta (incident angle) if refraction index of material is > 1.
-    auto material_left   = make_shared<dielectric>(1.50);                                         // glass
-    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);                                  // air bubble in glass
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);                  // fairly fuzzy gold
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    // Sphere Generation
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));     // plane sphere
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));     // new center sphere
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));       // glass sphere
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));     // air bubble sphere
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));      // gold metal sphere
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
-    // Camera Initialization, Ray Tracing, Rendering
+            // filter for where sphere is on x axis
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    // big glass sphere
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    // big matte sphere
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    // big metal sphere
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
-    cam.samples_per_pixel = 100;
-    cam.max_depth = 50;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 1200;
+    cam.samples_per_pixel = 500;
+    cam.max_depth         = 50;
 
-    cam.v_fov = 20;
-    cam.lookfrom = point3(-2,2,1);
-    cam.lookat   = point3(0,0,-1);
+    cam.v_fov    = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
     cam.vup      = vec3(0,1,0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist    = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist    = 10.0;
 
     cam.render(world);
-
-    return 0;
 }
